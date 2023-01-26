afd240f247731399d417b436ceb57c3c1ddcee9b
Yigit Yesilpinar <yigityesilpinar@gmail.com>
2023-01-26 19:39:48 +0100

    use dict instead of lis

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask_smorest import abort
 from uuid import uuid4
 
-venues = []
+venues = {}
+items = {}
 
 app = Flask(__name__)
 
@@ -13,21 +15,19 @@ def get_user_venues():
 
 @app.get("/venue/<string:id>")
 def get_user_venue_by_id(id: str):
-    for venue in venues:
-        print(f"Checking venue id: {venue['id']}")
-        print(f"for id: {id}")
-        if str(venue["id"]) == id:
-            return venue
-
-    return {}, 404
+    if id not in venues:
+        abort(404, message="Venue not found")
+    else:
+        return venues[id]
 
 
 @app.post("/venue")
 def create_venue():
     post_data = request.get_json()
     if post_data:
-        new_venue = {"id": uuid4(), "name": post_data["name"], "items": []}
-        venues.append(new_venue)
+        new_venue_id_uuid = uuid4()
+        new_venue = {"id": new_venue_id_uuid, "name": post_data["name"], "items": []}
+        venues[str(new_venue_id_uuid)] = new_venue
         return new_venue, 201
 
     return {}
